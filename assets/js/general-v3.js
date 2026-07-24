e7c85d3de277def8ce9e1e1cc54dd7ba3e46f4fb
Rudra Joshi <rudrajoshi@example.com>
2026-07-24 19:28:30 +0530

    Deploy debug safeGetFormDetails popup to inspect window context

--- assets/js/general-v3.js
+++ assets/js/general-v3.js
@@ -29,6 +29,7 @@ function safeGetFormDetails() {
   if (typeof getFormDetails === 'function') {
     return getFormDetails();
   }
+  alert("Debug safeGetFormDetails:\nwindow.getFormDetails = " + window.getFormDetails + "\ntypeof = " + (typeof window.getFormDetails) + "\nlocation = " + window.location.href);
   throw new Error("getFormDetails is not defined");
 }
 
